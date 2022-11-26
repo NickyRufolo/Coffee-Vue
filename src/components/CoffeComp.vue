5342d085c62a241df1141bbf6f2f51f9623d9207
NikiRufolo <nikirufolo@gmail.com>
2022-11-26 16:28:03 +0100

    v-for with axius COFFEE

---
+++ src/components/CoffeComp.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="coffee-card">
+        <img :src="propsCoffe.image" alt="">
+        <h2>{{propsCoffe.title}}</h2>
+    </div>
+    </template>
+    
+    
+    
+    <script>
+    export default {
+        name: 'CoffeComp',
+        props: {
+            propsCoffe : Object
+        }
+    }
+    </script>
+    
+    <!-- Add "scoped" attribute to limit CSS to this component only -->
+    <style scoped lang="scss">
+    .coffee-card {
+    width: calc(( 100% / 6 ) - 20px );
+    border: 1px solid rgb(12, 131, 52);
+    text-align: center;
+    margin: 10px 10px;
+    background-color:  rgb(12, 131, 52);
+    color: white;
+    }
+    .coffee-card > img {
+    width: 100%;
+    }
+    </style>
--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -1,21 +1,38 @@
 <template>
     <main>
-        <!-- Pasta -->
+        <!-- Coffee -->
         <div class="pre-Coffee">
             <i class="fa-regular fa-square"></i>
             <span class="pastatext">Our <strong>COFFEE</strong></span>
         </div>
-        <div class="Pasta-card-Div-Flex">
-            <PastaCard v-for="(element, index) in arrayPasta" :key="index" :propsPasta="element" />
+        <div class="Coffe-card-Div-Flex">
+            <CoffeComp v-for="(element, index) in dataCoffe" :key="index" :propsCoffe="element" />
         </div>
     </main>
 </template>
 
 <script>
+import CoffeComp from "./CoffeComp.vue"
+import axios from 'axios'
+
+
 export default {
     name: 'MainComp',
-    props: {
-        msg: String
+    components: {
+        CoffeComp,
+    },
+    data() {
+        return {
+        dataCoffe: ``
+        }
+    },
+    mounted() {
+        axios.get(`https://api.sampleapis.com/coffee/hot`)
+        .then ((response) => {
+            this.dataCoffe = response.data
+            // this.dataCoffe = recupero l'info dentro a data
+            // response.data.response recupero l'array di tutti i caffe presenti nel response nella chiamata GET dell'api
+        })
     }
 }
 </script>
@@ -26,5 +43,14 @@ export default {
     width: 80%;
     margin: 30px auto;
     font-size: 1.8rem;
+}
+
+.Coffe-card-Div-Flex {
+    display: flex;
+    width: 80%;
+    flex-wrap: wrap;
+}
+.pre-Coffee i {
+    margin-right: 20px;
 }
 </style> 
